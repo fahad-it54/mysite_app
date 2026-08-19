a821686f176e4246cb102647dd73179cc1f15242
Fahad <25bit090@gmail.com>
2026-08-19 23:26:26 +0400

    setting

--- mysite/settings.py
+++ mysite/settings.py
@@ -135,6 +135,16 @@ import os
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
+CSRF_TRUSTED_ORIGINS = [
+    'https://mysite-app-2.onrender.com',
+]
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+USE_X_FORWARDED_HOST = True
+
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+
 
 
 
