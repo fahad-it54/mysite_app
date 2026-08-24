8714df1c8306693a23b7d5cfd323b68787240dbc
Fahad <25bit090@gmail.com>
2026-08-25 00:26:48 +0400

    updated admin fix

--- mysite/settings.py
+++ mysite/settings.py
@@ -145,13 +145,13 @@ USE_X_FORWARDED_HOST = True
 CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 465
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER','fahadmohameda996@gmail.com')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')  
-DEFAULT_FROM_EMAIL = f'fahad Admissions <{EMAIL_HOST_USER}>'
+DEFAULT_FROM_EMAIL = f'fahad Admissions <fahadmohameda996@gmail.com>'
 
 
 
