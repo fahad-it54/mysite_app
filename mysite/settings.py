7e6e4388280f44f8c78da5f219924c76e454ad6a
Fahad <25bit090@gmail.com>
2026-08-26 10:31:12 +0400

    email change

--- mysite/settings.py
+++ mysite/settings.py
@@ -148,10 +148,11 @@ SESSION_COOKIE_SECURE = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 465
-EMAIL_USE_TLS = True
+EMAIL_USE_TLS = false 
+EMAIL_USE_SSL = True
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER','fahadmohameda996@gmail.com')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')  
-DEFAULT_FROM_EMAIL = f'fahad Admissions <fahadmohameda996@gmail.com>'
+DEFAULT_FROM_EMAIL = 'fahad Admissions <fahadmohameda996@gmail.com>'
 
 
 
